f80b68364cd030119d1a6b5c683dfbc692e509d6
Huzzky <vladislav.bychkov01@gmail.com>
2021-04-08 14:16:23 +0300

    added fullscreen func

--- src/components/Core/FullscreenBtn.jsx
+++ src/components/Core/FullscreenBtn.jsx
@@ -1,9 +1,20 @@
+import { useState } from 'react'
+import '../../assets/_fullscreen-logo.scss'
+
 export const FullscreenBtn = () => {
+  const [fullscreenBool, setFullscreenBool] = useState(false)
   return (
     <div
       className="api-console__header__fullscreen-btn-container"
       onClick={() => {
-        document.documentElement.requestFullscreen()
+        console.log(fullscreenBool)
+        if (!fullscreenBool) {
+          setFullscreenBool(true)
+          document.documentElement.webkitRequestFullScreen()
+        } else if (fullscreenBool) {
+          setFullscreenBool(false)
+          document.webkitCancelFullScreen()
+        }
       }}
     >
       <div className="api-console__header__fullscreen-btn--before"></div>
