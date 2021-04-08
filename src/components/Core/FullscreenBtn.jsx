53b0ef732d3748c23540aa3cd60edef1a2c246ab
Huzzky <vladislav.bychkov01@gmail.com>
2021-04-08 15:23:48 +0300

    added icon fullscreen css

--- src/components/Core/FullscreenBtn.jsx
+++ src/components/Core/FullscreenBtn.jsx
@@ -3,11 +3,13 @@ import '../../assets/_fullscreen-logo.scss'
 
 export const FullscreenBtn = () => {
   const [fullscreenBool, setFullscreenBool] = useState(false)
+
+  let fullScreenFuncMini = fullscreenBool ? 'after' : 'before'
+
   return (
     <div
       className="api-console__header__fullscreen-btn-container"
       onClick={() => {
-        console.log(fullscreenBool)
         if (!fullscreenBool) {
           setFullscreenBool(true)
           document.documentElement.webkitRequestFullScreen()
@@ -17,7 +19,45 @@ export const FullscreenBtn = () => {
         }
       }}
     >
-      <div className="api-console__header__fullscreen-btn--before"></div>
+      <div
+        className={
+          'api-console__header__fullscreen-btn  fullscreen-btn--' +
+          fullScreenFuncMini
+        }
+      >
+        <div
+          className={
+            'fullscreen-btn--' + fullScreenFuncMini + '__container-first'
+          }
+        >
+          <div
+            className={
+              'fullscreen-btn--' + fullScreenFuncMini + '__square-first'
+            }
+          ></div>
+          <div
+            className={
+              'fullscreen-btn--' + fullScreenFuncMini + '__square-second'
+            }
+          ></div>
+        </div>
+        <div
+          className={
+            'fullscreen-btn--' + fullScreenFuncMini + '__container-second'
+          }
+        >
+          <div
+            className={
+              'fullscreen-btn--' + fullScreenFuncMini + '__square-third'
+            }
+          ></div>
+          <div
+            className={
+              'fullscreen-btn--' + fullScreenFuncMini + '__square-fourth'
+            }
+          ></div>
+        </div>
+      </div>
     </div>
   )
 }
